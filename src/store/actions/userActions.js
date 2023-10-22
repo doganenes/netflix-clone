d984ce48e5a042f2311043c7e947988303226c5a
HEKİMCAN AKTAŞ <109082524+hekimm@users.noreply.github.com>
2023-10-22 16:42:35 +0300

    Add fi

--- src/store/actions/userActions.js
+++ src/store/actions/userActions.js
@@ -6,6 +6,7 @@ export const USER_SIGNUP_FAILURE = "USER_SIGNUP_FAILURE";
 export const USER_LOGIN_REQUEST = "USER_LOGIN_REQUEST";
 export const USER_LOGIN_SUCCESS = "USER_LOGIN_SUCCESS";
 export const USER_LOGIN_FAILURE = "USER_LOGIN_FAILURE";
+export const USER_LOGOUT = "USER_LOGOUT";
 
 // Aksiyon oluşturucuları
 export const userSignUpRequest = (userData) => ({
@@ -36,4 +37,8 @@ export const userLoginSuccess = (user) => ({
 export const userLoginFailure = (error) => ({
   type: USER_LOGIN_FAILURE,
   payload: error
+});
+
+export const userLogout = () => ({
+  type: USER_LOGOUT
 });
--- src/store/reducers/userReducers.js
+++ src/store/reducers/userReducers.js
@@ -4,7 +4,8 @@ import {
   USER_SIGNUP_FAILURE,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
-  USER_LOGIN_FAILURE
+  USER_LOGIN_FAILURE,
+  USER_LOGOUT
 } from "../actions/userActions";
 
 const initialState = {
@@ -24,6 +25,8 @@ export const userReducer = (state = initialState, action) => {
     case USER_SIGNUP_FAILURE:
     case USER_LOGIN_FAILURE:
       return { ...state, loading: false, error: action.payload };
+    case USER_LOGOUT:
+      return { ...state, user: null, error: null };
     default:
       return state;
   }
